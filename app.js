5ff7da02b85d3c30b29436ddf20d6f7ce222f6f5
SebastienFauque <61689730+SebastienFauque@users.noreply.github.com>
2021-07-12 17:32:58 -0700

    added a text input component and wrote psuedocode for code layout

--- app.js
+++ app.js
@@ -8,23 +8,134 @@
       // 3. This function creates an <iframe> (and YouTube player)
       //    after the API code downloads.
       var player;
-      function onYouTubeIframeAPIReady() {
-        player = new YT.Player('player', {
+      var videosArr = [
+        { // 1 thinking about robbin bank
           height: '390',
           width: '640',
           videoId: 'bp6CYzCQCYw',
           playerVars: {
-            'playsinline': 1
-            //'start': 60
-            //'end': 69
-            //'autoplay': 1
-            // 'playlist'
+            'playsinline': 1,
+            'start': 86,
+            'end': 90,
+            'autoplay': 1
           },
           events: {
             'onReady': onPlayerReady,
             'onStateChange': onPlayerStateChange
           }
-        });
+        },
+        { // 2 robbing bank
+          height: '390',
+          width: '640',
+          videoId: 'bp6CYzCQCYw',
+          playerVars: {
+            'playsinline': 1,
+            'start': 91,
+            'end': 100,
+            'autoplay': 1
+          },
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
+        },
+        { // 3 shoot with pan man
+          height: '390',
+          width: '640',
+          videoId: 'bp6CYzCQCYw',
+          playerVars: {
+            'playsinline': 1,
+            'start': 101,
+            'end': 106,
+            'autoplay': 1
+          },
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
+        },
+        { // 4 the first hanging after arrow to the neck
+          height: '390',
+          width: '640',
+          videoId: 'bp6CYzCQCYw',
+          playerVars: {
+            'playsinline': 1,
+            'start': 108,
+            'end': 112,
+            'autoplay': 1
+          },
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
+        },
+        { // 5 removed from tree,
+          height: '390',
+          width: '640',
+          videoId: 'jyeyeJZmSGU',
+          playerVars: {
+            'playsinline': 1,
+            'start': 1,
+            'end': 67,
+            'autoplay': 1
+          },
+          events: {
+          'onReady': onPlayerReady,
+          'onStateChange': onPlayerStateChange
+          }
+        },
+        { // Maybe 6 In town trial
+          height: '390',
+          width: '640',
+          videoId: 'jyeyeJZmSGU',
+          playerVars: {
+            'playsinline': 1,
+            'start': 109,
+            'end': 130,
+            'autoplay': 1
+          },
+          events: {
+          'onReady': onPlayerReady,
+          'onStateChange': onPlayerStateChange
+          }
+        },
+        { // 7 "first time?""
+          height: '390',
+          width: '640',
+          videoId: 'jyeyeJZmSGU',
+          playerVars: {
+            'playsinline': 1,
+            'start': 95,
+            'end': 121,
+            'autoplay': 1
+          },
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
+        },
+        { // 8 hood on then drop,
+          height: '390',
+          width: '640',
+          videoId: 'bp6CYzCQCYw',
+          playerVars: {
+            'playsinline': 1,
+            'start': 123,
+            'end': 127,
+            'autoplay': 1
+          },
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
+        }
+      ]
+      function onYouTubeIframeAPIReady() {
+        // add looping function around here to loop each video in
+        for (let i = 0; i < videosArr.length; i++) {
+          videoObj = videosArr[i];
+          player = new YT.Player('player', videoObj);
+        }
       }
 
       // 4. The API will call this function when the video player is ready.
@@ -38,7 +149,7 @@
       var done = false;
       function onPlayerStateChange(event) {
         if (event.data == YT.PlayerState.PLAYING && !done) {
-          setTimeout(stopVideo, 6000);
+          setTimeout(stopVideo, 12000);
           done = true;
         }
       }
@@ -46,9 +157,9 @@
         player.stopVideo();
       }
 
-      // make playlist with each clip or many clips from same video
+      //Done make playlist with each clip or many clips from same video
       // autoplay each clip
-      // stop clip after approx. 6 seconds
+      //Done stop clip after approx. 6 seconds
       // cycle to the next clip upon wrong answer
       // autoplay next clip
       // reset for each new game
