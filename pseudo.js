0412f0e0ba58279fff3bc3053ca4f64dfb5e858c
SebastienFauque <61689730+SebastienFauque@users.noreply.github.com>
2021-07-28 22:03:32 -0700

    tried putting down some thoughts using pseudo code

---
+++ pseudo.js
@@ -0,0 +1,61 @@
+// event listener on 'New Game' button that starts a new game
+
+// make a get secret word function
+    // define a regex that seeks out only letters
+    // Grab secret word and assign it to a tempVariable in JS
+    // update tempVariable with sanitized the secret word input using a regex and toLowerCase()
+    // check if the secret word is valid ()
+
+
+
+  // return the cleaned secret word and save it to a constant
+
+//make an Update function for the white text
+//inputsRevised: isGameStarted, correctGuessBoolean, isGameOverBoolean, isWinner
+  // if (isGameOverBoolean)
+    // if (isWinner)
+      // say "congrats"
+    // say "Game Started, pick a letter"
+  // if guess has been made
+    // if guess is correct
+      // say "good guess,  pick another letter"
+    // if guess is incorrect
+      // say "oops, pick again"
+
+
+      switch (gameState) {
+        case "Init":
+
+        case "started":
+            //listen for letter_Button
+        case "win":
+
+        case "lose":
+        case "correctGuess":
+        case "incorrectGuess":
+
+
+// function isGameOver()
+  // if incorrectGuesses is equal to Maximum incorrect Guesses
+    // say game over, you lose
+
+  // if correctGuessesWord is exactly equal to secretWord
+    // say 'you win'
+
+
+//(rewrite the underscore secret word)
+  // get the length of the secret word
+  // make an array of underscores that is the length of secret word
+  // replace the underscores with the underscoreArray
+
+
+// listen for letter buttons
+   // pass letter input to check letter function
+
+//make a check letter function
+//inputs letter
+    // if letter is correct
+        // call updateWhiteText(correct)
+        //
+
+    //
