e5925c6ba0c950a5a6fa573c5491e0d364b8b666
SebastienFauque <61689730+SebastienFauque@users.noreply.github.com>
2021-08-11 17:53:35 -0700

    updated the iframe player functionality. Now loops through each video upon incorrect guess and plays specific timestamps.

--- app.js
+++ app.js
@@ -2,11 +2,11 @@
 let currentIncorrectGuesses_span = document.getElementById("currentIncorrectGuesses"); //scoreboard incorrect guesses counter
 let textOutput_span = document.getElementById("textOutput"); // white text
 let correctGuessesWord_span = document.getElementById("correctGuessesWord"); // black text
-let secretWord_input = document.getElementById("secretWord"); // secret word from input box
+let secretWord_input = document.getElementById("secretWord"); // secret word input box
 let letter_buttons = document.getElementById("buttonsContainerID").value;
 
 //App Variables
-let incorrectGuesses = 0; // counter placeholder for currentIncorrectGuesses_span
+let incorrectGuesses = 0;
 let secretWordArray;
 let guessedLetters = [];
 
@@ -14,17 +14,38 @@ let guessedLetters = [];
 const newGame_button = document.getElementById("newGameButton");
 const words = {};
 const maxIncorrectGuesses = 8;
+const prefix = 'https://www.youtube-nocookie.com/embed/';
+const movieIds = ['bp6CYzCQCYw',
+                  'bp6CYzCQCYw',
+                  'qyqBAYCECZU',
+                  'bp6CYzCQCYw',
+                  'bp6CYzCQCYw',
+                  'hbEbD1Z_tNQ',
+                  'hbEbD1Z_tNQ',
+                  'hbEbD1Z_tNQ'
+                ];
+const suffixList = ['?&autoplay=1&start=86&end=90&modestbranding=1',
+                    '?&autoplay=1&start=91&end=100&modestbranding=1',
+                    '?&autoplay=1&start=35&end=65&modestbranding=1',
+                    '?&autoplay=1&start=101&end=107&modestbranding=1',
+                    '?&autoplay=1&start=108&end=112&modestbranding=1',
+                    '?&autoplay=1&start=0&end=15&modestbranding=1',
+                    '?&autoplay=1&start=31&end=45&modestbranding=1',
+                    '?&autoplay=1&start=65&end=75&modestbranding=1'
+                  ];
+
 
 
 // Start webpage modifiers
 textOutput_span.innerHTML = 'Welcome to the game of Hangman! Type a secret word in text box to start. Only letters will be saved. ';//update white text
+document.getElementById('player').style.display = "none";
 
 // Trigger a new game
 newGame_button.addEventListener('click', function() {
-  // words.secretWord = secretWord;
   words.secretWord = getAndCheckSecretWord();
-  //set incorrect number and letter counters to 0 and display and previously guessed letters
+  //set incorrect number and letter counters to 0 and display and add previously guessed letters
   incorrectGuesses = 0;
+  document.getElementById('player').style.display = "";
   document.getElementById('buttonsContainerID').style.display = "";
   guessedLetters.forEach((elem) => {document.getElementById(elem).style.display = "";})
   guessedLetters = [];
@@ -40,13 +61,11 @@ function getAndCheckSecretWord() {
     textOutput_span.innerHTML = `The entered word '${givenSecretWord}' was not valid, insert a new word and click "New Game".`
     return;
   } else if (secretWordArray.length) {
-    // update the text insert box (secret word input)
     secretWord_input.innerHTML = document.getElementById("secretWord").value = '';
     // update black text
     correctGuessesWord_span.innerHTML = '-'.repeat(secretWordArray.length);
     // update white text
     textOutput_span.innerHTML = `OK! Pick a letter.`
-    // let validatedSecretWord = matches.join('').toLowerCase(); // lowercase string
     correctGuesses = '-'.repeat(secretWordArray.length);
     correctGuessesWord_span.innerHTML = correctGuesses;
     return secretWordArray.join('');
@@ -57,23 +76,20 @@ const chooseLetter = function(letter) {
   guessedLetters.push(letter);
 
   if(isCorrectGuess(words.secretWord, letter)) {
-    // hide chosen letter
     document.getElementById(letter).style.display = "none";
     // update blackText
     correctGuesses = updateCorrectGuesses(secretWordArray, letter, correctGuesses);
-    // check if game is over
     if(isGameOver(words.secretWord, correctGuesses, incorrectGuesses)) return;
-    // update whiteText
     textOutput_span.innerHTML = `You correctly guessed '${letter}', guess another letter!`;
 
   } else {
-    // hide chosen letter
+    //set up and play next video
+    document.getElementById('player').src = prefix+movieIds[incorrectGuesses]+suffixList[incorrectGuesses];
     document.getElementById(letter).style.display = "none";
-    // increase scoreboard incorrect guesses by 1;
     incorrectGuesses++;
     currentIncorrectGuesses_span.innerHTML = incorrectGuesses;
+
     if (isGameOver(words.secretWord, correctGuesses, incorrectGuesses)) return;
-    // update whiteText
     textOutput_span.innerHTML = `You incorrectly guessed '${letter}', guess another letter!`;
   }
 
@@ -112,175 +128,3 @@ function updateCorrectGuesses(secretWordArray, letter, correctGuesses) {
   correctGuessesWord_span.innerHTML = correctGuesses;
   return correctGuesses;
 }
-
-
-
-//----------------------
-// video player
-
-//
-// videoPlayer.js
-// 2. This code loads the IFrame Player API code asynchronously.
-var tag = document.createElement('script');
-
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-      // 3. This function creates an <iframe> (and YouTube player)
-//    after the API code downloads.
-var player;
-var videosArr = [
-  { // 1 thinking about robbin bank
-    height: '390',
-    width: '640',
-    videoId: 'bp6CYzCQCYw',
-    playerVars: {
-      'playsinline': 1,
-      'start': 86,
-      'end': 90,
-      'autoplay': 1
-    },
-    events: {
-      'onReady': onPlayerReady,
-      'onStateChange': onPlayerStateChange
-    }
-  },
-  { // 2 robbing bank
-    height: '390',
-    width: '640',
-    videoId: 'bp6CYzCQCYw',
-    playerVars: {
-      'playsinline': 1,
-      'start': 91,
-      'end': 100,
-      'autoplay': 1
-    },
-    events: {
-      'onReady': onPlayerReady,
-      'onStateChange': onPlayerStateChange
-    }
-  },
-  { // 3 shoot with pan man
-    height: '390',
-    width: '640',
-    videoId: 'bp6CYzCQCYw',
-    playerVars: {
-      'playsinline': 1,
-      'start': 101,
-      'end': 106,
-      'autoplay': 1
-    },
-    events: {
-      'onReady': onPlayerReady,
-      'onStateChange': onPlayerStateChange
-    }
-  },
-  { // 4 the first hanging after arrow to the neck
-    height: '390',
-    width: '640',
-    videoId: 'bp6CYzCQCYw',
-    playerVars: {
-      'playsinline': 1,
-      'start': 108,
-      'end': 112,
-      'autoplay': 1
-    },
-    events: {
-      'onReady': onPlayerReady,
-      'onStateChange': onPlayerStateChange
-    }
-  },
-  { // 5 removed from tree,
-    height: '390',
-    width: '640',
-    videoId: 'jyeyeJZmSGU',
-    playerVars: {
-      'playsinline': 1,
-      'start': 1,
-      'end': 67,
-      'autoplay': 1
-    },
-    events: {
-    'onReady': onPlayerReady,
-    'onStateChange': onPlayerStateChange
-    }
-  },
-  { // Maybe 6 In town trial
-    height: '390',
-    width: '640',
-    videoId: 'jyeyeJZmSGU',
-    playerVars: {
-      'playsinline': 1,
-      'start': 109,
-      'end': 130,
-      'autoplay': 1
-    },
-    events: {
-    'onReady': onPlayerReady,
-    'onStateChange': onPlayerStateChange
-    }
-  },
-  { // 7 "first time?""
-    height: '390',
-    width: '640',
-    videoId: 'jyeyeJZmSGU',
-    playerVars: {
-      'playsinline': 1,
-      'start': 95,
-      'end': 121,
-      'autoplay': 1
-    },
-    events: {
-      'onReady': onPlayerReady,
-      'onStateChange': onPlayerStateChange
-    }
-  },
-  { // 8 hood on then drop,
-    height: '390',
-    width: '640',
-    videoId: 'bp6CYzCQCYw',
-    playerVars: {
-      'playsinline': 1,
-      'start': 123,
-      'end': 127,
-      'autoplay': 1
-    },
-    events: {
-      'onReady': onPlayerReady,
-      'onStateChange': onPlayerStateChange
-    }
-  }
-]
-
-function onYouTubeIframeAPIReady() {
-  // add looping function around here to loop each video in
-  for (let i = 0; i < videosArr.length; i++) {
-    videoObj = videosArr[i];
-    player = new YT.Player('player', videoObj);
-  }
-}
-// 4. The API will call this function when the video player is ready.
-function onPlayerReady(event) {
-  event.target.playVideo();
-}
-
-// 5. The API calls this function when the player's state changes.
-//    The function indicates that when playing a video (state=1),
-//    the player should play for six seconds and then stop.
-var done = false;
-function onPlayerStateChange(event) {
-  if (event.data == YT.PlayerState.PLAYING && !done) {
-    setTimeout(stopVideo, 12000);
-    done = true;
-  }
-}
-function stopVideo() {
-  player.stopVideo();
-}
-
-// function clickA()
-//       hover color
-//       userChoice_div.classList.add('red-glow');
-
-// On start game button click, run the playGame function
